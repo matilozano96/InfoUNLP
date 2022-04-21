38c74be1a2ce83ddf50042035d7b3cabddc865fd
matilozano96 <matilozano96@hotmail.com>
2022-04-21 16:59:02 -0300

    cosas

--- TP2022_ProyectoAlumnos/src/tema1/Ej04Oficinas.java
+++ TP2022_ProyectoAlumnos/src/tema1/Ej04Oficinas.java
@@ -1,20 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package tema1;
 
-/**
- *
- * @author matil
- */
-public class Ej04Oficinas {
+import PaqueteLectura.Lector;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Ej04Oficinas {
     public static void main(String[] args) {
-        // TODO code application logic here
+        int pisos = 4;
+        int ofi = 2;
+        
+        int [][] matriz = new int[pisos][4];
+        
+        System.out.print("Numero de piso (1-"+ pisos + "): ");
+        int num = Lector.leerInt();
+        int num2;
+        while (num < pisos+1) { 
+            if (num > 0) { 
+                System.out.print("Numero de oficina (1-"+ ofi + "): ");
+                num2 = Lector.leerInt();	
+                if ((num2 > 0)&(num2 < ofi+1)) { matriz[num - 1][num2 - 1] += 1; }
+                else { System.out.println("Invalido (1-"+ ofi + ")"); }
+            }
+            else { System.out.println("Invalido (1-"+ pisos + ")"); }
+            
+            System.out.print("Numero de piso (1-"+ pisos + "): ");
+            num = Lector.leerInt();
+        }
+        
+        for(int i=0; i < pisos; i++){
+            System.out.println("Piso " + (i+1) + ": ");
+            for(int j=0; j < ofi; j++){
+                System.out.println("    Oficina " + (j+1) + ": " + matriz[i][j]);
+            }
+        }
     }
-    
 }
