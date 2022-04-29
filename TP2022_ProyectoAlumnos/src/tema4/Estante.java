dc99cbb7f5a82f0be49eb1e33be26f51e7b19100
matilozano96 <matilozano96@hotmail.com>
2022-04-28 22:42:14 -0300

    Detalles

--- TP2022_ProyectoAlumnos/src/tema4/Estante.java
+++ TP2022_ProyectoAlumnos/src/tema4/Estante.java
@@ -1,6 +1,6 @@
 package tema4;
 public class Estante {
-    private int DF = 20;
+    private int DF = 2;
     private Libro [] libros;
     private int DL = 0;
     
@@ -15,14 +15,15 @@ public class Estante {
         return (DF == DL);
     }
     public void agregarLibro(Libro l){
-        if (DL < DF) libros[DL++]=l;
-        else System.out.println("Error. Se alcanzó la cantidad maxima de libros.");
+        if (estaLleno()) System.out.println("Error. Se alcanzó la cantidad maxima de libros.");
+        else libros[DL++]=l;
     }
     public Libro buscarLibro(String t) {
-        for (int i=0; i < DL; i++){  
-            if (libros[i].getTitulo().equals(t)) return libros[i];
-        }
-        return null;
+        int i;
+        for (i=0; (i < DL) && !(libros[i].getTitulo().equals(t)); i++){}
+        
+        if (i == DL) return null;
+        return libros[i];
     }
     
 }
