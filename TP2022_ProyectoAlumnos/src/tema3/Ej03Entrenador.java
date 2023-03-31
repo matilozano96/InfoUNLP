af2ce5db1defd0c3e3b575ce02742dc769614798
matilozano96 <matilozano96@hotmail.com>
2023-03-31 20:57:16 -0300

    Deleting things

--- TP2022_ProyectoAlumnos/src/tema3/Ej03Entrenador.java
+++
@@ -1,9 +0,0 @@
-package tema3;
-public class Ej03Entrenador {
-    public static void main(String[] args) {
-        Entrenador e = new Entrenador();
-        
-        System.out.println(e.toString());
-    }
-    
-}
